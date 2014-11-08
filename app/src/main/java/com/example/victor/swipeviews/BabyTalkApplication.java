ad42505e521c811973b3c8937ad877491e2a811f
kazakovv <kazakov.vcictor@gmail.com>
2014-11-08 14:09:16 +0000

    Parse pushes working

--- app/src/main/java/com/example/victor/swipeviews/BabyTalkApplication.java
+++ app/src/main/java/com/example/victor/swipeviews/BabyTalkApplication.java
@@ -7,6 +7,7 @@ import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParsePush;
+import com.parse.PushService;
 import com.parse.SaveCallback;
 
 
@@ -17,16 +18,8 @@ public class BabyTalkApplication extends Application {
         super.onCreate();
         Parse.initialize(this, "q8kuy8UbICvlNOFsPLNCAMTTFyrcLbmC1jNKLddU", "ApdaaWEaZSNdob8maokZyxRHJlQA6BoOfWdh9y3L");
         ParseInstallation.getCurrentInstallation().saveInBackground();
-        ParsePush.subscribeInBackground("Victor", new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e != null) {
-                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
-                } else {
-                    Log.e("com.parse.push", "failed to subscribe for push", e);
-                }
-            }
-        });
+        PushService.setDefaultPushCallback(this, Main.class);
 
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 }
